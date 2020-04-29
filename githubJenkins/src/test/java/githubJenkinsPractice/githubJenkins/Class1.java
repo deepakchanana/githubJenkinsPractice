08523cbe5f4c542089d40de4d55b1d8c06e65b5c
deepakchanana <deepakdeep1992@gmail.com>
2020-04-29 16:20:00 -0700

    first time code push

---
+++ githubJenkins/src/test/java/githubJenkinsPractice/githubJenkins/Class1.java
@@ -0,0 +1,15 @@
+package githubJenkinsPractice.githubJenkins;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+
+	@Test
+	public void tc()
+	{
+		System.out.println("I am testcase");
+		System.out.println("changes done");
+		System.out.println("again changessssshhs");
+		
+	}
+}
