5332a7050feb5e84d6d6e387ee4c1ef2de692921
deepakchanana <deepakdeep1992@gmail.com>
2020-05-02 09:36:21 -0700

    Changes done to the file

--- githubJenkins/src/test/java/githubJenkinsPractice/githubJenkins/Class1.java
+++ githubJenkins/src/test/java/githubJenkinsPractice/githubJenkins/Class1.java
@@ -7,10 +7,7 @@ public class Class1 {
 	@Test
 	public void tc()
 	{
-		System.out.println("I am testcase");
-		System.out.println("changes done");
-		System.out.println("again changessssshhs");
-		System.out.println("my changes done");
+		System.out.println("This is api batch");
 		
 	}
 }
